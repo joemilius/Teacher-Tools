445058c731fd52b9eec86f63895e9f0a2f3aa946
Joe Milius <joemilius@gmail.com>
2023-06-10 09:53:31 -0600

    minutes-plus event listener and adding function created and functional

--- src/timer.js
+++ src/timer.js
@@ -1,6 +1,6 @@
 let timerContent = document.querySelector("#timer-content")
 let timerHeader = document.querySelector('#timer-container h3')
-
+// Timer Collapse //
 timerHeader.addEventListener('click', (event) => {
     if(timerContent.className === 'hide'){
         timerContent.className = ''
@@ -11,3 +11,25 @@ timerHeader.addEventListener('click', (event) => {
     }
     
 })
+
+// Minutes Buttons Events //
+let minutesText = document.querySelector('#minutes')
+let minutesPlus = document.querySelector('#minutes-plus')
+let minutesMinus = document.querySelector('#minutes-minus')
+let minuteTotal = 0
+
+minutesPlus.addEventListener('click', (event)=> {
+    minuteAdd()
+})
+
+function minuteAdd(){
+    minuteTotal++
+    if (minuteTotal < 10){
+        minutesText.textContent = `0${minuteTotal}`
+    } else if (minuteTotal >= 10 && minuteTotal <= 99){
+        minutesText.textContent = minuteTotal
+    }else if (minuteTotal > 99){
+        minuteTotal--
+        minutesText.textContent = 99
+    }
+}
