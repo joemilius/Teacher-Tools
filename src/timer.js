59d376bea7474822c237d5227cb1667870d9ce14
Joe Milius <joemilius@gmail.com>
2023-06-11 18:18:03 -0600

    seconds add button functional

--- src/timer.js
+++ src/timer.js
@@ -45,7 +45,30 @@ function minutesSubtract(){
     } else if (minuteTotal >= 10 && minuteTotal <= 99){
         minutesText.textContent = minuteTotal
     }else if (minuteTotal > 99){
-        minuteTotal--
+        minuteTotal++
         minutesText.textContent = 99
+    }
+}
+
+// Seconds Buttons Events //
+let secondsText = document.querySelector('#seconds')
+let secondsPlus = document.querySelector('#seconds-plus')
+let secondsMinus = document.querySelector('#seconds-minus')
+let secondsTotal = 0
+
+secondsPlus.addEventListener('click', (event) => {
+    secondsAdd()
+})
+
+function secondsAdd(){
+    secondsTotal++
+    if (secondsTotal < 10){
+        secondsText.textContent = `0${secondsTotal}`
+    } else if (secondsTotal >= 10 && secondsTotal <= 59 ){
+        secondsText.textContent = secondsTotal
+    } else if (secondsTotal > 59){
+        secondsText.textContent = '00'
+        secondsTotal = 0
+        minutesAdd()
     }
 }
