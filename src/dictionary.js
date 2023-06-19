62c3a8986dbcfc6bb87f2e977577e70d4987100d
Joe Milius <joemilius@gmail.com>
2023-06-19 16:22:26 -0600

    fetch for dictionary form active

--- src/dictionary.js
+++ src/dictionary.js
@@ -12,3 +12,16 @@ dictionaryHeader.addEventListener('click', (event) => {
         dictionaryHeader.textContent = "Dictionary ▶️"
     }
 })
+
+// Word Look Up //
+
+let wordInput = document.querySelector('#word-input')
+let wordForm = document.querySelector('#word-form')
+
+wordForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let word = wordInput.value 
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+})
